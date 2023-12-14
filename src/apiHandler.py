c35f87cb9dcdfdb4c44b89770fb9c4cee1b8d801
Loïc Dörr <loic.debeuf@gmail.com>
2023-12-14 14:12:01 +0100

    edit something

---
+++ src/apiHandler.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from mySQLService import MySQLService
+
+app = FastAPI()
+mySQLService = MySQLService()
+
+@app.get("/")
+def read_root():
+  return 'Hello'
+
+@app.get('/statistics/{business_id}')
+def getStatistics(business_id: str):
+  return mySQLService.statistics(business_id)
